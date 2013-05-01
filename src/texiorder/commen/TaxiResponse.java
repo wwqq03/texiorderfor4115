1008baba686121f42334235d87bdee4b53eda007
Qi <wwqq03@gmail.com>
2013-05-01 13:08:39 +0200

    update

--- src/texiorder/commen/TaxiResponse.java
+++ src/texiorder/commen/TaxiResponse.java
@@ -3,6 +3,7 @@ package texiorder.commen;
 public class TaxiResponse extends TaxiMessage {
 	
 	private String ack;
+	private String customer;
 
 	public String getAck() {
 		return ack;
@@ -10,6 +11,14 @@ public class TaxiResponse extends TaxiMessage {
 
 	public void setAck(String ack) {
 		this.ack = ack;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
 	}
 
 }
